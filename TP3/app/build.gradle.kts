7b2bc9847164e9d31b27ddbdee4d095306e8b801
Noah Tilleul <noah.tilleul@etu.unilim.fr>
2025-02-11 11:20:23 +0100

    Le TP3 peut juste pas compiler avec tous les trucs de donnés et le compilateur m'aide pas sur l'erreur

--- TP3/app/build.gradle.kts
+++ TP3/app/build.gradle.kts
@@ -40,8 +40,9 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.material3.android)
+    implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
-    implementation(libs.androidx.navigation.compose)
 }
